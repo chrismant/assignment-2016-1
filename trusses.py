9db479b5fdf544d42457306c96c7f03ad9543021
chrismant <v-ch.mantikas@windowslive.com>
2016-03-18 22:07:01 +0200

    Update trusses.py

--- trusses.py
+++ trusses.py
@@ -1,2 +1,9 @@
-trusses 
-  {
+def generate_edges(graph):
+    edges = []
+    for node in graph:
+        for neighbour in graph[node]:
+            edges.append((node, neighbour))
+
+    return edges
+
+print(generate_edges(graph))
